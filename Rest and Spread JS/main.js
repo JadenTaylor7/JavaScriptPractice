945c609b10e6f9e5c91f29c92959adc0dbd64fd0
Jaden Taylor <jtjadent@gmail.com>
2023-03-13 14:30:57 -0600

    Finishing rest and spread

--- Rest and Spread JS/main.js
+++ Rest and Spread JS/main.js
@@ -1,3 +1,4 @@
+
 //When we don't know how many parameters to pass
 function sum(...args) { 
     let total = 0;
@@ -23,19 +24,36 @@ function purchaseTotal(...themItems) {
     return total;
 }
 
-let soap = 3.5
-let protienShakeSingle = 4
-let sleepGummySample = 3
-let toothpaste = 3.43
-let doubleX = 56
-let protienPowder = 36
+let soap = 3.5;
+let protienShakeSingle = 4;
+let sleepGummySample = 3;
+let toothpaste = 3.43;
+let doubleX = 56;
+let protienPowder = 36;
 
 console.log("The shopping total is: $" + purchaseTotal(soap,toothpaste,doubleX));
 
 
 function people(name, ...moreNames) {
-    console.log("Praying for"+name, moreNames.join(" "));
+    console.log("Praying for "+name, moreNames.join(" "));
 }
 
 
 people("Jordan","Bryli","Joseph","Colter","Taylin");
+
+
+
+//using spread
+let disHuuuuuugeArray = [21,4,5,6,3,4,2.1,9,12.4];
+console.log("Each number displayed separately is");
+console.log(...disHuuuuuugeArray);
+
+
+
+
+function display(theKey, theValue) {
+    return{key: theKey, value: theValue};
+}
+
+const someDictionary = display(...["this is the key", "this is the value"]);
+console.log(someDictionary);
